1e6172f7aaab29abba90ddcfc6f8f6c4bae3d2cd
Olatunji Adeniyi <neyoo2078@yahoo.com>
2023-08-05 02:21:59 +0100

    baseurl commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     ZEGOAPP_ID: 1002377906,
     ZEGO_SERVER_SECRET: '2da4ae4831126ac6be2a72ae02461da0',
-    BaseUrl: 'http://localhost:5000',
+    BaseUrl: 'https://whatapp-server-mgyi.onrender.com',
   },
   images: {
     domains: ['localhost'],
